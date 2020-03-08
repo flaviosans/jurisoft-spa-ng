cbe91146d104a6aaa292de823f909bd41a3184d8
Flávio Santos <flaviosantos.dev@gmail.com>
2020-03-08 12:58:42 -0300

    Pessoa salva emite um evento

--- src/app/pessoa/pessoa-edit/pessoa-edit.component.ts
+++ src/app/pessoa/pessoa-edit/pessoa-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pessoa } from 'src/app/models/pessoa/pessoa';
 import { PessoaService } from 'src/app/services/pessoa.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-pessoa-edit',
@@ -13,20 +13,18 @@ export class PessoaEditComponent implements OnInit {
   pessoa: Pessoa = {} as Pessoa;
 
   constructor(private pessoaService: PessoaService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(p => {
       this.pessoaService.getPessoa(p.get('id')).subscribe(p => {
         this.pessoa = p;
-      })
+      });
     });
   }
 
-  public salvar(): void {
-    this.pessoaService.savePessoa(this.pessoa).subscribe(p => {
-      alert('funciona')
-    });
+  public voltar(){
+    this.router.navigate(['/pessoas']);
   }
-
 }
